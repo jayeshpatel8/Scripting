f3451f51137843ba9d03c6f8eabfc1b6556e8ba5
jayeshpatel8 <jnpatel81@gmail.com>
2019-08-08 17:53:30 +0200

    Example test.c
    This file has trace function called inside function My_test_funciton,

---
+++ Bash/Awk/test.c
@@ -0,0 +1,6 @@
+trace_printf(" sdfsdsdfsdf  sdfs fs s");
+void My_test_funciton(void* p, int q,char arr)
+{
+trace_printf("123 123123 213123 2354234 345345 345345");
+  TRACE_INFO(ABCDE_2343, "sadfs  sdf23423 4356345 7654sdf sdfs %d %d %d \n",sdf,sdfs, 2543);
+}
